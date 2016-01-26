66c9e9e591f7388d9430a73323ee2ffd88b9f6b2
Kevin Ballard <kevin.ballard@postmates.com>
2016-01-25 17:38:52 -0800

    Add an implementation of SipHash

---
+++ Source/SipHash.swift
@@ -0,0 +1,211 @@
+//
+//  SipHash.swift
+//  PostmatesNetworking
+//
+//  Created by Kevin Ballard on 1/25/16.
+//  Copyright © 2016 Postmates. All rights reserved.
+//
+
+/// A generic hasher that implements SipHash-2-4.
+/// Once a hasher is created, data can be added to it iteratively.
+/// Once all data has been added, call `finish()` to get the resulting value.
+/// After the hasher has been finalized, it resets to the initial state and can
+/// be reused to hash new data.
+///
+/// - Important: Although the SipHash algorithm is considered to be cryptographically strong,
+///   this implementation has not been reviewed for such purposes and is almost certainly insecure.
+internal struct SipHasher: OutputStreamType {
+    /// Creates a new `SipHasher`.
+    /// - Parameter key: A pair of 64-bit integers to use as the key. Defaults to `(0,0)`.
+    ///   This pair of integers is equivalent to a 128-bit key where the first element is
+    ///   the low 64 bits and the second element is the high 64 bits.
+    init(key: (UInt64, UInt64) = (0,0)) {
+        k0 = key.0
+        k1 = key.1
+        v0 = k0 ^ 0x736f6d6570736575
+        v1 = k1 ^ 0x646f72616e646f6d
+        v2 = k0 ^ 0x6c7967656e657261
+        v3 = k1 ^ 0x7465646279746573
+    }
+    
+    // key
+    let k0: UInt64
+    let k1: UInt64
+    // hash state
+    // v0, v2 and v1, v3 show up in pairs in the algorithm.
+    // order them that way here in case the compiler can pick up some simd optimizations automatically
+    var v0: UInt64
+    var v2: UInt64
+    var v1: UInt64
+    var v3: UInt64
+    /// Number of bytes processed
+    var b: UInt = 0
+    /// Number of unprocessed bytes at the end.
+    var tail: UInt64 = 0
+    /// Number of bytes in `tail` that are valid. Guaranteed to be less than 8.
+    var tailLen: UInt = 0
+    
+    mutating func write<C: CollectionType where C.Generator.Element == UInt8>(bytes: C) {
+        var count: UInt = numericCast(bytes.count)
+        b += count
+        var startIndex = bytes.startIndex
+        @inline(__always) func u8to64_le(inout idx: C.Index, len: UInt) -> UInt64 {
+            var acc: UInt64 = 0
+            for i in 0..<len {
+                acc |= UInt64(bytes[idx]) << UInt64(8 * i)
+                idx = idx.successor()
+            }
+            return acc
+        }
+        if tailLen != 0 {
+            let needed = 8 - tailLen
+            tail |= u8to64_le(&startIndex, len: min(count, needed)) << UInt64(8 * tailLen)
+            if count < needed {
+                tailLen += count
+                return
+            } else {
+                compress(tail)
+                // NB: tail and tailLen are reset later
+                count -= needed
+            }
+        }
+        for _ in 0..<(count / 8) {
+            compress(u8to64_le(&startIndex, len: 8))
+        }
+        tailLen = count % 8
+        if tailLen > 0 {
+            tail = u8to64_le(&startIndex, len: tailLen)
+        }
+    }
+    
+    mutating func finish() -> UInt64 {
+        defer { self = SipHasher(key: (k0, k1)) }
+        if tailLen == 0 {
+            tail = 0
+        }
+        tail |= UInt64(b) << 56
+        compress(tail)
+        v2 ^= 0xff
+        sipRound()
+        sipRound()
+        sipRound()
+        sipRound()
+        return v0 ^ v1 ^ v2 ^ v3
+    }
+    
+    @inline(__always) private mutating func compress(m: UInt64) {
+        v3 ^= m
+        sipRound()
+        sipRound()
+        v0 ^= m
+    }
+    
+    @inline(__always) private mutating func sipRound() {
+        @inline(__always) func rotl(x: UInt64, _ n: UInt64) -> UInt64 {
+            return (x << n) | (x >> (64 &- n))
+        }
+        v0 = v0 &+ v1
+        v1 = rotl(v1, 13)
+        v1 ^= v0
+        
+        v2 = v2 &+ v3
+        v3 = rotl(v3, 16)
+        v3 ^= v2
+        
+        v0 = rotl(v0, 32)
+        
+        v2 = v2 &+ v1
+        v1 = rotl(v1, 17)
+        v1 ^= v2
+        
+        v0 = v0 &+ v3
+        v3 = rotl(v3, 21)
+        v3 ^= v0
+        
+        v2 = rotl(v2, 32)
+    }
+    
+    // MARK: Convenience methods
+    
+    /// Writes an `Int8` to the `SipHasher`.
+    mutating func write(n: Int8) {
+        write(CollectionOfOne(UInt8(bitPattern: n)))
+    }
+    
+    /// Writes the little-endian representation of an `Int16` to the `SipHasher`.
+    mutating func write(n: Int16) {
+        var n = n.littleEndian
+        withUnsafePointer(&n) { ptr in
+            write(UnsafeBufferPointer(start: UnsafePointer(ptr), count: sizeofValue(n)))
+        }
+    }
+    
+    /// Writes the little-endian representation of an `Int32` to the `SipHasher`.
+    mutating func write(n: Int32) {
+        var n = n.littleEndian
+        withUnsafePointer(&n) { ptr in
+            write(UnsafeBufferPointer(start: UnsafePointer(ptr), count: sizeofValue(n)))
+        }
+    }
+    
+    /// Writes the little-endian representation of an `Int64` to the `SipHasher`.
+    mutating func write(n: Int64) {
+        var n = n.littleEndian
+        withUnsafePointer(&n) { ptr in
+            write(UnsafeBufferPointer(start: UnsafePointer(ptr), count: sizeofValue(n)))
+        }
+    }
+    
+    /// Writes a `UInt8` to the `SipHasher`.
+    mutating func write(n: UInt8) {
+        write(CollectionOfOne(n))
+    }
+    
+    /// Writes the little-endian representation of a `UInt16` to the `SipHasher`.
+    mutating func write(n: UInt16) {
+        var n = n.littleEndian
+        withUnsafePointer(&n) { ptr in
+            write(UnsafeBufferPointer(start: UnsafePointer(ptr), count: sizeofValue(n)))
+        }
+    }
+    
+    /// Writes the little-endian representation of a `UInt32` to the `SipHasher`.
+    mutating func write(n: UInt32) {
+        var n = n.littleEndian
+        withUnsafePointer(&n) { ptr in
+            write(UnsafeBufferPointer(start: UnsafePointer(ptr), count: sizeofValue(n)))
+        }
+    }
+    
+    /// Writes the little-endian representation of a `UInt64` to the `SipHasher`.
+    mutating func write(n: UInt64) {
+        var n = n.littleEndian
+        withUnsafePointer(&n) { ptr in
+            write(UnsafeBufferPointer(start: UnsafePointer(ptr), count: sizeofValue(n)))
+        }
+    }
+    
+    /// Writes a `Bool` to the `SipHasher`, as a single byte of value `0` or `1`.
+    mutating func write(b: Bool) {
+        let n: UInt8 = b ? 1 : 0
+        write(n)
+    }
+    
+    /// Writes the UTF-16 representation of `string` to the hasher.
+    /// - Note: This does not take canonical equivalence into account.
+    mutating func write(string: String) {
+        for x in string.utf16 {
+            write(x)
+        }
+    }
+    
+    /// Writes the `UInt32` representation of `scalar` to the hasher.
+    mutating func write(scalar: UnicodeScalar) {
+        write(scalar.value)
+    }
+    
+    /// Writes the string representation of `c` to the hasher.
+    mutating func write(c: Character) {
+        c.writeTo(&self)
+    }
+}
---
+++ Tests/SipHashTests.swift
@@ -0,0 +1,72 @@
+//
+//  SipHashTests.swift
+//  PostmatesNetworking
+//
+//  Created by Kevin Ballard on 1/25/16.
+//  Copyright © 2016 Postmates. All rights reserved.
+//
+
+import XCTest
+@testable import PMHTTP
+
+class SipHashTests: XCTestCase {
+    func testSipHash() {
+        // test data taken from https://github.com/veorq/SipHash/blob/master/main.c (public domain)
+        // SipHash-2-4 output with
+        // k = 00 01 02 ...
+        // and
+        // in = (empty string)
+        // in = 00 (1 byte)
+        // in = 00 01 (2 bytes)
+        // in = 00 01 02 (3 bytes)
+        // ...
+        // in = 00 01 02 ... 3e (63 bytes)
+        let expected: [UInt64] = [
+            0x310e0edd47db6f72, 0xfd67dc93c539f874, 0x5a4fa9d909806c0d, 0x2d7efbd796666785,
+            0xb7877127e09427cf, 0x8da699cd64557618, 0xcee3fe586e46c9cb, 0x37d1018bf50002ab,
+            0x6224939a79f5f593, 0xb0e4a90bdf82009e, 0xf3b9dd94c5bb5d7a, 0xa7ad6b22462fb3f4,
+            0xfbe50e86bc8f1e75, 0x903d84c02756ea14, 0xeef27a8e90ca23f7, 0xe545be4961ca29a1,
+            0xdb9bc2577fcc2a3f, 0x9447be2cf5e99a69, 0x9cd38d96f0b3c14b, 0xbd6179a71dc96dbb,
+            0x98eea21af25cd6be, 0xc7673b2eb0cbf2d0, 0x883ea3e395675393, 0xc8ce5ccd8c030ca8,
+            0x94af49f6c650adb8, 0xeab8858ade92e1bc, 0xf315bb5bb835d817, 0xadcf6b0763612e2f,
+            0xa5c91da7acaa4dde, 0x716595876650a2a6, 0x28ef495c53a387ad, 0x42c341d8fa92d832,
+            0xce7cf2722f512771, 0xe37859f94623f3a7, 0x381205bb1ab0e012, 0xae97a10fd434e015,
+            0xb4a31508beff4d31, 0x81396229f0907902, 0x4d0cf49ee5d4dcca, 0x5c73336a76d8bf9a,
+            0xd0a704536ba93e0e, 0x925958fcd6420cad, 0xa915c29bc8067318, 0x952b79f3bc0aa6d4,
+            0xf21df2e41d4535f9, 0x87577519048f53a9, 0x10a56cf5dfcd9adb, 0xeb75095ccd986cd0,
+            0x51a9cb9ecba312e6, 0x96afadfc2ce666c7, 0x72fe52975a4364ee, 0x5a1645b276d592a1,
+            0xb274cb8ebf87870a, 0x6f9bb4203de7b381, 0xeaecb2a30b22a87f, 0x9924a43cc1315724,
+            0xbd838d3aafbf8db7, 0x0b1a2a3265d51aea, 0x135079a3231ce660, 0x932b2846e4d70666,
+            0xe1915f5cb1eca46c, 0xf325965ca16d629f, 0x575ff28e60381be5, 0x724506eb4c328a95
+            ].map({UInt64(bigEndian: $0)})
+        let key: (UInt64, UInt64) = (0x0706050403020100, 0x0f0e0d0c0b0a0908)
+        var buf = ContiguousArray<UInt8>(count: 64, repeatedValue: 0)
+        for i in 0..<buf.count {
+            buf[i] = UInt8(i)
+        }
+        for (i, exp) in expected.enumerate() {
+            var hasher = SipHasher(key: key)
+            // hash as a single write
+            do {
+                hasher.write(buf[0..<i])
+                let result = hasher.finish()
+                XCTAssert(result == exp, "input of length \(i): 0x\(String(result, radix: 16)) is not equal to 0x\(String(exp, radix:16))")
+            }
+            // hash as two writes
+            do {
+                hasher.write(buf[0..<(i/2)])
+                hasher.write(buf[(i/2)..<i])
+                let result = hasher.finish()
+                XCTAssert(result == exp, "input of length \(i): 0x\(String(result, radix: 16)) is not equal to 0x\(String(exp, radix:16))")
+            }
+            // hash as one write per byte
+            do {
+                for j in 0..<i {
+                    hasher.write(buf[j])
+                }
+                let result = hasher.finish()
+                XCTAssert(result == exp, "input of length \(i): 0x\(String(result, radix: 16)) is not equal to 0x\(String(exp, radix:16))")
+            }
+        }
+    }
+}
