db4ebee959abb11a35f141938df9f45a279d52a9
Kevin Ballard <kevin.ballard@postmates.com>
2016-03-03 12:06:32 -0800

    Add method to parse the "Date" header as an NSDate
    This is a static method on `HTTPManager` and parses the "Date" header
    out of an `NSURLResponse`. There's a companion method that takes the
    header value directly, for use with other HTTP-date headers.

---
+++ Source/DateParsing.swift
@@ -0,0 +1,82 @@
+//
+//  DateParsing.swift
+//  PostmatesNetworking
+//
+//  Created by Kevin Ballard on 3/3/16.
+//  Copyright © 2016 Postmates. All rights reserved.
+//
+
+import Foundation
+
+public extension HTTPManager {
+    /// Parses the `Date` header from a URL response and returns it.
+    ///
+    /// - Parameter response: An `NSURLResponse` that the header is pulled from. If this
+    ///   is not an `NSHTTPURLResponse`, `nil` is returned.
+    /// - Returns: An `NSDate`, or `nil` if the header doesn't exist or has an invalid format.
+    static func parsedDateHeaderFromResponse(response: NSURLResponse) -> NSDate? {
+        guard let response = response as? NSHTTPURLResponse,
+            dateString = response.allHeaderFields["Date"] as? String
+            else { return nil }
+        return parsedDateHeaderFromString(dateString)
+    }
+    
+    /// Parses a header value that is formatted like the "Date" HTTP header.
+    ///
+    /// This parses the specific format allowed for the "Date" header, and any
+    /// other header that uses the `HTTP-date` production.
+    ///
+    /// See [section 3.3.1 of RFC 2616][RFC] for details.
+    ///
+    /// [RFC]: https://www.w3.org/Protocols/rfc2616/rfc2616-sec3.html#sec3.3.1
+    ///
+    /// - Parameter dateString: The string value of the HTTP header.
+    /// - Returns: An `NSDate`, or `nil` if `dateString` contains an invalid format.
+    static func parsedDateHeaderFromString(dateString: String) -> NSDate? {
+        return rfc1123DateFormatter.dateFromString(dateString)
+            ?? rfc850DateFormatter.dateFromString(dateString)
+            ?? asctimeDateFormatter.dateFromString(dateString)
+    }
+    
+    private static let posixLocale = NSLocale(localeIdentifier: "en_US_POSIX")
+    private static let gregorianCalendar: NSCalendar = {
+        let calendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
+        calendar.timeZone = NSTimeZone(forSecondsFromGMT: 0)
+        return calendar
+    }()
+    private static let rfc1123DateFormatter: NSDateFormatter = {
+        let formatter = NSDateFormatter()
+        formatter.locale = posixLocale
+        formatter.calendar = gregorianCalendar
+        formatter.timeZone = gregorianCalendar.timeZone
+        formatter.dateFormat = "EEE',' dd MMM yyyy HH':'mm':'ss 'GMT'"
+        formatter.lenient = false
+        return formatter
+    }()
+    private static let rfc850DateFormatter: NSDateFormatter = {
+        let formatter = NSDateFormatter()
+        formatter.locale = posixLocale
+        formatter.calendar = gregorianCalendar
+        formatter.timeZone = gregorianCalendar.timeZone
+        formatter.dateFormat = "EEEE',' dd'-'MMM'-'yy HH':'mm':'ss 'GMT'"
+        formatter.lenient = false
+        // From RFC 2616 Section 19.3 Tolerant Applications:
+        // > HTTP/1.1 clients and caches SHOULD assume that an RFC-850 date
+        // > which appears to be more than 50 years in the future is in fact
+        // > in the past (this helps solve the "year 2000" problem).
+        formatter.twoDigitStartDate = gregorianCalendar.dateByAddingUnit(.Year, value: -49, toDate: NSDate(), options: [])
+        return formatter
+    }()
+    private static let asctimeDateFormatter: NSDateFormatter = {
+        let formatter = NSDateFormatter()
+        formatter.locale = posixLocale
+        formatter.calendar = gregorianCalendar
+        formatter.timeZone = gregorianCalendar.timeZone
+        // NB: asctime specifies day as ( 2DIGIT | ( SP 1DIGIT ) ). There's no way to represent this with a
+        // date format, ICU seems to treat stretches of consecutive whitespace all as a single space, so this should
+        // still parse just fine. Luckily we don't have to generate these strings.
+        formatter.dateFormat = "EEE MMM dd HH':'mm':'ss yyyy"
+        formatter.lenient = false
+        return formatter
+    }()
+}
