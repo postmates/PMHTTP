0e3b0a55119746985a155364520d5dcea9a321da
Kevin Ballard <kevin.ballard@postmates.com>
2016-04-19 14:45:21 -0700

    Add a license to the project
    This project is licensed under the Apache License, Version 2.0 or the
    MIT license.

--- Tests/PMHTTPRetryTests.swift
+++ Tests/PMHTTPRetryTests.swift
@@ -3,7 +3,13 @@
 //  PostmatesNetworking
 //
 //  Created by Kevin Ballard on 4/1/16.
-//  Copyright © 2016 Postmates. All rights reserved.
+//  Copyright © 2016 Postmates.
+//
+//  Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+//  http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+//  <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+//  option. This file may not be copied, modified, or distributed
+//  except according to those terms.
 //
 
 import XCTest
