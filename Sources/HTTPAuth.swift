4c2073e7f01e67873840f04ecb391c2e193cf0b4
Kevin Ballard <kevin@sb.org>
2017-12-21 22:07:26 -0800

    Add license header comment to files that were missing it

--- Sources/HTTPAuth.swift
+++ Sources/HTTPAuth.swift
@@ -5,6 +5,12 @@
 //  Created by Kevin Ballard on 2/7/17.
 //  Copyright © 2017 Postmates. All rights reserved.
 //
+//  Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+//  http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+//  <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+//  option. This file may not be copied, modified, or distributed
+//  except according to those terms.
+//
 
 import Foundation
 
