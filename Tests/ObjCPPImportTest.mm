12872aaa30fc5fdb22ec5007f661cb64cc2d8b11
Kevin Ballard <kevin@sb.org>
2018-05-17 19:06:27 -0700

    Add a test file to ensure Obj-C++ can import <PMHTTP/PMHTTP.h>

---
+++ Tests/ObjCPPImportTest.mm
@@ -0,0 +1,17 @@
+//
+//  ObjCPPImportTest.m
+//  PMHTTPTests
+//
+//  Created by Kevin Ballard on 5/17/18.
+//  Copyright © 2018 Postmates.
+//
+//  Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+//  http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+//  <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+//  option. This file may not be copied, modified, or distributed
+//  except according to those terms.
+//
+
+// This file exists just to make sure we can import the PMHTTP framework header from Obj-C++.
+
+#import <PMHTTP/PMHTTP.h>
